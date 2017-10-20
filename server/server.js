6257dc1248599f210a4b502182a9b34edd552e3a
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-20 18:11:16 -0500

    git push overwrote with koala holla, fixing

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
-var koalaRouter = require('./routes/koala_router.js');
+var taskRouter = require('./routes/task_route.js');
+var taskCounter = require('./routes/task_counter.js');
 var poolModule = require('./modules/pool.js');
 var pool = poolModule;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
-app.use('/koalas', koalaRouter);
+app.use('/tasklist', taskRouter);
+app.use('/taskcounter', taskCounter);
 
 // Start listening for requests on a specific port
 app.listen(port, function(){
