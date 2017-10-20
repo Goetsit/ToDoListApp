179b1984b1228d446dafe1cc9018478f90c66a7a
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-20 18:39:57 -0500

    add task route

---
+++ server/routes/task_add.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var pg = require('pg');
+
+var router = express.Router();
+var config = {
+  database: 'deneb',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000
+};
+
+var pool = new pg.Pool(config);
+
+router.post('/',function(req,res){
+    var task = req.body;
+    console.log('TASK ON POST', task);
+  pool.connect(function(errorConnectingToDB, db, done) {
+    if(errorConnectingToDB){
+        console.log('Error connecting to db', errorConnectingToDB);
+        res.sendStatus(500);
+    } else {
+        var queryText = 'INSERT INTO "task" ("task", "duedate") VALUES ($1, $2);';
+        db.query(queryText, [task.task, task.duedate], function (errorMakingQuery, result) {
+        done();
+        if(errorMakingQuery){
+          console.log('Error making query', errorMakingQuery, result)
+          res.sendStatus(500);
+        } else {
+          console.log(result.rows);
+          res.send(result.rows);
+        }
+      });
+    }
+  });
+});
+
+
+module.exports = router;
