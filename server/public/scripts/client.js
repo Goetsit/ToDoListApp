c0f86954289b91061bf379e603a66648dcf491fd
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-20 18:14:52 -0500

    git push over wrote with koala holla, fixing

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,187 +1,73 @@
-console.log( 'js' );
-
-$( document ).ready(readyNow);
-
-function readyNow(){
-  console.log( 'JQ' );
-  // load existing koalas on page load
-  getKoalas();
-  // add koala button click
-  $( '#addButton' ).on( 'click', addButtonClick);
-  $( '#viewKoalas' ).on('click', '.delete', deleteKoala);
-  $( '#viewKoalas' ).on('click', '.transfer', markReadyForTransfer);
-  $( '#viewKoalas').on('click', '.editKoalas', editKoalasFunction)
-}
+console.log('js');
 
 var editing = false;
 var editingId = 0;
 
-function addButtonClick(){
-  console.log( 'in addButton on click' );
-  var name = $('#nameIn').val();
-  var age = $('#ageIn').val();
-  var gender = $('#genderIn').val();
-  var readyForTransfer = $('#readyForTransferIn').val();
-  var notes = $('#notesIn').val();
-  // get user input and put in an object
-  // NOT WORKING YET :(
-  // using a test object
-var formComplete = true;
-if (name === '') {
-  formComplete = false;
-}
-if (age === '') {
-  formComplete = false;
-}
-if (gender === '') {
-  formComplete = false;
-}
-if (readyForTransfer === '') {
-  formComplete = false;
-}
-if (notes === '') {
-  formComplete = false;
+
+$(document).ready(readyNow);
+
+function readyNow() {
+  console.log('JQ');
+  getTasks();
+  getCount();
+  $('#addBtn').on('click', addTask);
+
 }
 
-if (formComplete) {
-  var objectToSend = {
-    name: name,
-    age: age,
-    gender: gender,
-    readyForTransfer: readyForTransfer,
-    notes: notes
-  }
-
-  if(editing === true){
-    editing = false;
-    $('#status').text('Add Koalas');
-    sendEdits();
-  } else {
-    // call saveKoala with the new obejct
-    saveKoala( objectToSend );
-  }
-  $('input').val('');
-} else {
-  alert("Please complete the form");
-}
-}
-
-// GET AJAX call to server to get koalas
-function getKoalas(){
-  console.log( 'in getKoalas' );
+function getTasks() {
   $.ajax({
     type: 'GET',
-    url: '/koalas'
-  }).done(function(response){
-    console.log( 'got some koalas: ', response );
+    url: '/tasklist'
+  }).done(function (response) {
+    console.log('getting tasks: ', response);
     //Append to dom function
     appendToDom(response);
-  }).fail(function(error){
+  }).fail(function (error) {
     console.log('GET failed:', error);
   })
 }
 
-// POST AJAX call to server to add new Koala
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
+
+
+
+function getCount() {
   $.ajax({
-    type: 'POST',
-    url: '/koalas',
-    data: newKoala
-  }).done(function(response){
-    console.log('Success:', response);
-    getKoalas();
-  }).fail(function(error){
-    console.log('Failed:',error);
+    type: 'GET',
+    url: '/taskcounter'
+  }).done(function (response) {
+    console.log('getting task count: ', response);
+    appendCounter(response);
+  }).fail(function (error) {
+    console.log('GET failed:', error);
   })
+  
 }
 
-//DELETE AJAX call to remove a koala
-function deleteKoala(){
-  id = $(this).closest('tr').data('koala').id;
-  console.log(id);
-  $.ajax({
-    method: 'DELETE',
-    url: '/koalas/' + id
-  }).done(function(response){
-    getKoalas();
-  }).fail(function(error){
-    console.log('Error deleting', error);
-  })
-}
-
-function appendToDom(koalas) {
-  $('#viewKoalas').empty();
-  for(var i=0; i<koalas.length; i++){
-    if(koalas[i].readyfortransfer === false){
-      var button = '<button class="btn btn-success transfer">Ready for transfer</button>';
-      //var koala = koalas[i];
-    } else {
-      button = '';
-    }
-    var $tr = $('<tr data-id="' + koalas[i].id + '"><td>' + koalas[i].name + '</td><td>' + koalas[i].age + '</td><td>' + koalas[i].gender + '</td><td>' + koalas[i].readyfortransfer + '</td><td>' + koalas[i].notes + '</td><td>' + button + '</td><td>' + '<button class="btn btn-secondary editKoalas">Edit</button>' + '</td><td>' + '<button class="btn btn-danger delete">Delete</button>' + '</td></tr>');
-    $tr.data('koala', koalas[i]);
-    $('#viewKoalas').append($tr);
+function appendCounter(count){
+  console.log(count[0]);
+  for(var ii = 0; ii < count.length; ii += 1){
+    var counter = count[ii]
+    console.log('append counter test',counter.count);
+    $('#count').html(counter.count);
   }
 }
 
-function markReadyForTransfer() {
-  id = $(this).closest('tr').data('koala').id;
-  console.log(id);
-  $.ajax({
-    method: 'PUT',
-    url: '/koalas/ready/' + id,
-  }).done(function(response){
-    getKoalas();
-  }).fail(function(error){
-    console.log('Error marking ready for transfer', error);
-  })
-}
+function appendToDom(tasks) {
 
-function editKoalasFunction(){
+  console.log(tasks);
 
-  var name = $(this).closest('tr').data('koala').name;
-  var age = $(this).closest('tr').data('koala').age;
-  var gender = $(this).closest('tr').data('koala').gender;
-  var readyForTransfer = $(this).closest('tr').data('koala').readyfortransfer;
-  var notes = $(this).closest('tr').data('koala').notes;
-  editingId = $(this).closest('tr').data('koala').id;
-  editing = true;
-  $('#status').text('Editing Mode!');
-
-  $('#nameIn').val(name);
-  $('#ageIn').val(age);
-  $('#genderIn').val(gender);
-  $('#readyForTransferIn').val(readyForTransfer);
-  $('#notesIn').val(notes);
-}; // end PUT ajax
-
-function sendEdits() {
-  var name = $('#nameIn').val();
-  var age = $('#ageIn').val();
-  var gender = $('#genderIn').val();
-  var readyForTransfer = $('#readyForTransferIn').val();
-  var notes = $('#notesIn').val();
-  
-  var updateKoala = {
-    name: name,
-    age: age,
-    gender: gender,
-    readyForTransfer: readyForTransfer,
-    notes: notes
+  $('#viewTask').empty();
+  // Loop through products and append to dom
+  for (var i = 0; i < tasks.length; i += 1) {
+    var task = tasks[i];
+    var $tr = $('<tr></tr>');
+    //$tr.data('task', tasks);
+    $tr.append('<td>' + task.task + '</td>');
+    $tr.append('<td>' + task.duedate + '</td>');
+    $tr.append('<td>' + task.complete + '</td>');
+    $('#viewTask').append($tr);
   }
 
-  console.log('sendEdits and the editingID is:', editingId);
-  console.log('This is the updateKoala:', updateKoala);
-  
-  $.ajax({
-    method: 'PUT',
-    url: '/koalas/' + editingId,
-    data: updateKoala
-  }).done(function(response){
-    console.log('send edits function response:', response)
-    getKoalas();
-  }).fail(function(error){
-    console.log('error getting update koalas back:' , error)
-  })
+
+
 }
