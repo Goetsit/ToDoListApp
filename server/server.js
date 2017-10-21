f43c0a6c2382be9360f3b93c7d63da81e75b62b6
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-21 13:07:06 -0500

    added task complete router

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
 var taskRouter = require('./routes/task_route.js');
 var taskCounter = require('./routes/task_counter.js');
+var addTask = require('./routes/task_add.js');
+var taskComplete = require('./routes/task_complete.js');
 var poolModule = require('./modules/pool.js');
 var pool = poolModule;
 
@@ -11,6 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 app.use('/tasklist', taskRouter);
 app.use('/taskcounter', taskCounter);
+app.use('/addtask', addTask);
+app.use('/tasklist/complete', taskComplete);
 
 // Start listening for requests on a specific port
 app.listen(port, function(){
