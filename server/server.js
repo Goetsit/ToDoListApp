4ddbbe90c0e5ed05300b524a737d95bdd8e467d8
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-21 17:11:10 -0500

    added note functionality

--- server/server.js
+++ server/server.js
@@ -5,7 +5,10 @@ var port = process.env.PORT || 5000;
 var taskRouter = require('./routes/task_route.js');
 var taskCounter = require('./routes/task_counter.js');
 var addTask = require('./routes/task_add.js');
+var taskNote = require('./routes/task_note.js');
 var taskComplete = require('./routes/task_complete.js');
+var taskDelete= require('./routes/task_delete.js');
+var taskNoteAlert= require('./routes/task_note_alert.js');
 var poolModule = require('./modules/pool.js');
 var pool = poolModule;
 
@@ -15,6 +18,9 @@ app.use('/tasklist', taskRouter);
 app.use('/taskcounter', taskCounter);
 app.use('/addtask', addTask);
 app.use('/tasklist/complete', taskComplete);
+app.use('/tasklist/delete', taskDelete);
+app.use('/tasklist/note', taskNote);
+app.use('/tasklist/note/alert/', taskNoteAlert);
 
 // Start listening for requests on a specific port
 app.listen(port, function(){
