6e07447b147768acd215f48470dccda8bfef5275
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-21 19:40:50 -0500

    making sure everything is up to date, adding comments

--- server/routes/task_router.js
+++ server/routes/task_router.js
@@ -9,7 +9,7 @@ var config = {
   max: 10,
   idleTimeoutMillis: 30000
 };
-
+//Initial GET request, selects from the "task" table
 var pool = new pg.Pool(config);
 
 router.get('/',function(req,res){
@@ -18,7 +18,7 @@ router.get('/',function(req,res){
         console.log('Error connecting to db', errorConnectingToDB);
         res.sendStatus(500);
     } else {
-      var queryText = 'SELECT t.taskid, t.task, t.duedate, t.complete FROM "task" t ORDER BY t."duedate" ASC;';
+      var queryText = 'SELECT t.taskid, t.task, t.duedate, t.complete FROM "task" t ORDER BY t."complete", t."duedate" ASC;';
       db.query(queryText, function(errorMakingQuery, result){
         done();
         if(errorMakingQuery){
